22ccb219658d4aff564423eb061cf9c834380792
andy <a.chaa@marketinvoice.com>
2016-03-16 15:36:29 +0000

    no dev tool

--- main.js
+++ main.js
@@ -28,8 +28,7 @@ app.on('ready', function() {
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/views/index.html');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
