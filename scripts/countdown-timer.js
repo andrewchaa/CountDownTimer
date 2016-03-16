a321dc65daa3290c8579bbd5b6abc37dee8c2894
andy <a.chaa@marketinvoice.com>
2016-03-16 16:21:23 +0000

    removed day and hour, made it smaller

--- scripts/countdown-timer.js
+++ scripts/countdown-timer.js
@@ -6,12 +6,10 @@ function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
   return {
     'total': t,
-    'days': days,
-    'hours': hours,
+    'days': 0,
+    'hours': 0,
     'minutes': minutes,
     'seconds': seconds
   };
@@ -19,16 +17,12 @@ function getTimeRemaining(endtime) {
 
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
   var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
